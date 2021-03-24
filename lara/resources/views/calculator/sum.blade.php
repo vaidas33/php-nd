9e111f605f401f6d7fd00f8756d145958cdfaef0
vaidas33 <73877931+vaidas33@users.noreply.github.com>
2021-03-24 10:47:10 +0200

    fix spaces

--- lara/resources/views/calculator/sum.blade.php
+++ lara/resources/views/calculator/sum.blade.php
@@ -1,14 +1,9 @@
 @extends('layout.main')
-
 @section('sum')
+    <h1>Skaičiuojam:</h1>
 
-<h1>Skaičiuojam:</h1>
-
-@if(is_numeric($pirms) && is_numeric($antrs))
-Pirmas: {{ $pirms }} Antras: {{ $antrs }} Suma: {{ $result}}
-
-@endif
-
+    @if(is_numeric($pirms) && is_numeric($antrs))
+    Pirmas: {{ $pirms }} Antras: {{ $antrs }} Suma: {{ $result}}
+    @endif
 @endsection
-
 @section('title') Calculator @endsection
